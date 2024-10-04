1271b737890bcc807c1dbba5bae053d553976184
aspizu <aspizu@protonmail.com>
2024-10-04 22:46:08 +0530

    Change NO_ROADS.MON to not be infinitely looping.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 set -x
 gcc $(cat compile_flags.txt) *.c -o libmonotone &&\
-valgrind ./libmonotone songs/SWINGIN1.MON output.pcm &&\
+valgrind ./libmonotone songs/NO_ROADS.MON output.pcm &&\
 ffmpeg -y -f u8 -ar 44100 -ac 2 -i output.pcm output.mp3
